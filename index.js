cfb654628952aac1f365919a5b0d8062c1bf2531
Harry <harrybest800@gmail.com>
2021-04-20 20:05:20 +0100

    hello world

---
+++ index.js
@@ -0,0 +1,6 @@
+function drawRect() {
+    const canvas = document.getElementById('canvas')
+    let ctx = canvas.getContext('2d')
+    ctx.fillStyle = 'rgb(200, 0, 0)'
+    ctx.fillRect(10,10,50,50)
+}
